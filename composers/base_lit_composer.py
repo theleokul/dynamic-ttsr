bugfix: memory leakage in logging (no detaching) + averaging stats at the end of epoch

composers/base_lit_composer.py
@@ -209,7 +209,15 @@ class BaseLitComposer(pl.LightningModule, abc.ABC):
         raise NotImplementedError()
 
     def training_epoch_end(self, outputs):
-        output = {f'avg_{k}': torch.as_tensor([o[k] for o in outputs]).mean() for k in outputs[0].keys()}
+        keys = set()
+        # NOTE: Check keys only in first 5 outputs by default (Override if more needed)
+        for o in outputs[:5]:
+            keys |= set(o.keys())
+        output = {
+            f'avg_{k}': \
+            torch.as_tensor([o[k] for o in outputs if o.get(k, None) is not None]).mean() \
+            for k in keys
+        }
         self.log_dict(output, prog_bar=True)
 
     def validation_epoch_end(self, outputs):

composers/ttsr_lit_composer.py
@@ -75,11 +75,7 @@ class FullTTSRLitComposer(TTSRLitComposer):
                 d_fake = self.discriminator(fake)
                 loss += self.loss['adv'](d_fake, discriminator=False) * self.loss['coef_adv']
             self.log('loss_g', loss, sync_dist=True)
-
-            return {
-                'loss': loss
-                , 'loss_g': loss
-            }
+            output = {'loss': loss, 'loss_g': loss.detach()}
         elif optimizer_idx == 1:  # Discriminator
             fake, real = sr, hr
             fake_detach = fake.detach()
@@ -92,8 +88,13 @@ class FullTTSRLitComposer(TTSRLitComposer):
             d_hat = self.discriminator(hat)
             loss = self.loss['adv'](d_fake, d_real, d_hat, fake, real, hat, discriminator=True)
             self.log('loss_d', loss, sync_dist=True)
+            output = {'loss': loss, 'loss_d': loss.detach()}
 
-            return {
-                'loss': loss
-                , 'loss_d': loss
-            }
+        return output
+
+        def training_epoch_end(self, outputs):
+            keys = ['loss_d', 'loss_g']
+            output = {}
+
+            output = {f'avg_{k}': torch.as_tensor([o[k] for o in outputs]).mean() for k in outputs[0].keys()}
+            self.log_dict(output, prog_bar=True)

loss.py
@@ -70,7 +70,9 @@ class WGANGPAdversarialLoss(nn.Module):
             loss_d = (d_fake - d_real).mean()
             gradients = torch.autograd.grad(
                 outputs=d_hat.sum(), inputs=hat,
-                retain_graph=True, create_graph=True, only_inputs=True
+                retain_graph=True
+                , create_graph=True
+                , only_inputs=True
             )[0]
             gradients = gradients.view(gradients.size(0), -1)
             gradient_norm = gradients.norm(2, dim=1)